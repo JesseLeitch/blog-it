bbe93200763241f7b4d5100c22504792ba6b4234
Jesse Leitch <jesseleitch@cmail.carleton.ca>
2014-08-15 13:10:22 -0400

    New s3 setup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,8 +19,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAISNZFMD3S6ZSDBEQ',            # required
-    :aws_secret_access_key  => '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',     # required
+    :aws_access_key_id      => ENV['S3_KEY']            # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],     # required
     :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'blog-it'               # required
